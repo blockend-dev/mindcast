1e196a547c3e6b4165b1e1f203fa8b7c47678ed7
blockend-dev <162776165+blockend-dev@users.noreply.github.com>
2025-11-07 04:22:25 +0000

    successful deployment

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,5 +1,6 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import '@nomicfoundation/hardhat-verify'
 import * as dotenv from 'dotenv'
 import 'hardhat-deploy'
 import '@typechain/hardhat'
@@ -7,6 +8,7 @@ dotenv.config()
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY!
 const OG_TESTNET_RPC = process.env.OG_TESTNET_RPC!
+const OG_MAINNET_RPC = process.env.OG_MAINNET_RPC!
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -16,7 +18,10 @@ const config: HardhatUserConfig = {
       optimizer: {
         enabled: true,
         runs: 200
-      }
+      },
+       metadata: {
+        bytecodeHash: "none", 
+      },
     }
   },
   networks: {
@@ -25,9 +30,40 @@ const config: HardhatUserConfig = {
       chainId: 16602,
       accounts: [PRIVATE_KEY!],
       saveDeployments : true
+    },
+    OG_mainnet: {
+      url: OG_MAINNET_RPC,
+      chainId: 16661,
+      accounts: [PRIVATE_KEY!],
+      saveDeployments : true
     }
   },
-
+ etherscan: {
+    apiKey: {
+      og_testnet: process.env.ETHERSCAN_API_KEY!, // Use a placeholder if you don't have one
+      og_mainnet: process.env.ETHERSCAN_API_KEY!  // Use a placeholder if you don't have one
+    },
+    customChains: [
+      {
+        // Testnet
+        network: "testnet",
+        chainId: 16602,
+        urls: {
+          apiURL: "https://chainscan-galileo.0g.ai/open/api",
+          browserURL: "https://chainscan-galileo.0g.ai",
+        },
+      },
+      {
+        // Mainnet
+        network: "mainnet",
+        chainId: 16661,
+        urls: {
+          apiURL: "https://chainscan.0g.ai/open/api",
+          browserURL: "https://chainscan.0g.ai",
+        },
+      },
+    ],
+  },
   namedAccounts: {
     deployer: {
       default: 0,
